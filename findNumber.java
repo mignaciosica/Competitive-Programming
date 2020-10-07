64d18733ec107c3d8ec28da676dfb887d5476ae3
P4ssenger <mignacio.sica@gmail.com>
2020-10-07 19:08:02 -0300

    FindNumber update

--- findNumber.java
+++ findNumber.java
@@ -1,8 +1,10 @@
-int findNumber (int[]numbs){
-	n = numbs.length + 1;
-	sum = n * (n+1) / 2;
-	for(int i = 0; i < n - 1; i++){
-		sum -= numbs[i];
+public class FindNumber{
+	static int findNumber (int[]numbs){
+		int n = numbs.length + 1;
+		int sum = n * (n+1) / 2;
+		for(int i = 0; i < n - 1; i++){
+			sum -= numbs[i];
+		}
+		return sum;
 	}
-	return sum;
 }
---
+++ findNumber.java~
@@ -0,0 +1,8 @@
+int findNumber (int[]numbs){
+	n = numbs.length + 1;
+	sum = n * (n+1) / 2;
+	for(int i = 0; i < n - 1; i++){
+		sum -= numbs[i];
+	}
+	return sum;
+}
